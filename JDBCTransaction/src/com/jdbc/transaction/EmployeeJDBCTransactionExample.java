2c0fec4b156e9c6688e1c1f4ef5a4904ef6996b6
Thanh <thanhphamnl@gmail.com>
2020-05-04 21:42:40 -0700

    Initial commit JDBCTransaction

---
+++ JDBCTransaction/src/com/jdbc/transaction/EmployeeJDBCTransactionExample.java
@@ -0,0 +1,41 @@
+package com.jdbc.transaction;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class EmployeeJDBCTransactionExample {
+
+	public static void main(String[] args) {
+		
+		Connection con = null;
+		try {
+			con = DBConnection.getConnection();
+			
+			//set auto commit to false
+			con.setAutoCommit(false);
+
+		        EmployeeJDBCInsertExample.insertEmployeeData(con, 2, "Thanh");
+		        EmployeeJDBCInsertExample.insertAddressData(con, 2, "Albany Dr", "JDBC Transaction rolled back successfully", "USA");
+			
+			 //now commit transaction
+			 con.commit();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			try {
+				con.rollback();
+				System.out.println("JDBC Transaction rolled back successfully");
+			} catch (SQLException e1) {System.out.println("SQLException in rollback"+e.getMessage());
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (con != null)
+					con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
